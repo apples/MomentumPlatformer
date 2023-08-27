2a04f5a4d8093bf7c7d105779a89677b561d9a80
WiredOverload <mike.everywhere@gmail.com>
2023-08-27 02:32:37 -0500

    new menu and music

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -8,13 +8,24 @@ public class MusicController : MonoBehaviour
 {
     private PlayerControls controls;
 
+    [SerializeField] private AudioSource introAudioSource;
     [SerializeField] private AudioSource audioSource;
+    private bool inIntro = true;
 
     private void Awake()
     {
         controls = new PlayerControls();
         controls.Special.Enable();
         controls.Special.MuteMusic.performed += ToggleMute;
+    }
+
+    public void Update()
+    {
+        if(inIntro && !introAudioSource.isPlaying)
+        {
+            inIntro = false;
+            audioSource.Play();
+        }
     }
 
     private void ToggleMute(InputAction.CallbackContext obj)
